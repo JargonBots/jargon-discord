0b69e97a81d88877d1f4cd71a19567573b5c4870
Armaan <103453421+ak2k2@users.noreply.github.com>
2022-12-26 01:30:58 -0500

    import conversation into discord message thread

--- discord/direct_message.py
+++ discord/direct_message.py
@@ -1,5 +1,12 @@
+# Property of JargonBots.
+# Written By Armaan Kapoor & Eric (Dan) Karlsson on 12-25-2022.
+
 import discord
 import os
+from dotenv import load_dotenv
+from kcjargon.openai.conversation import conversation_obj
+
+load_dotenv("/Users/armaan/Desktop/kcjargon-main/config/.env")
 
 intents = discord.Intents.default()
 intents.members = True
@@ -16,9 +23,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
-        await message.author.send('Hi there!')
-        await message.author.send('What can I do for you?')
+    if message.content.startswith('!'):
+        Name = str(message.content[1::])
+        cv = conversation(starting_environment="The following is chat with {}.".format(Name), bot_name="{}: ".format(Name))
+        await message.author.send("The following is a chat with {}".format(Name))
+        await message.author.send(message.content)
         response = await client.wait_for('message')
         print(f'Reponse: {response}')
         await message.author.send('Got it! I will work on it right away.')
