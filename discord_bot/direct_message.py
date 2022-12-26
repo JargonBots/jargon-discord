6f166065680d58130dc331cd432d69bbdcafde12
Armaan <armaani.kapoor@gmail.com>
2022-12-26 05:36:21 -0500

    Running Demo

--- discord_bot/direct_message.py
+++ discord_bot/direct_message.py
@@ -7,6 +7,12 @@ import discord
 
 # from kcjargon.openai.conversation import conversation_obj
 from dotenv import load_dotenv
+import sys
+from os import path
+jargonai_dir = "/jargonai/jargonai"
+if path.isdir(jargonai_dir):
+    sys.path.insert(0, jargonai_dir)
+    from conversation import Conversation
 
 load_dotenv()
 
@@ -25,17 +31,33 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
     if message.author == client.user:
         return
+    # actor_name = None
+    # cv = None
+    in_conversation = False
 
     if message.content.startswith("!"):
-        Name = str(message.content[1::])
-        # cv = conversation(starting_environment="The following is chat with {}.".format(Name), bot_name="{}: ".format(Name))
-        await message.author.send("The following is a chat with {}".format(Name))
-        await message.author.send(message.content)
+        in_conversation = True
+        actor_name = str(message.content[1::])
+        cv = Conversation(starting_environment="The following is chat with {}.".format(actor_name), bot_name="{}: ".format(actor_name))
+        # await message.author.send("The following is a chat with {}".format(Name))
+        # await message.author.send(message.content)
+    
+    while message.content != "quit" and in_conversation:
+        cv.environment += "\nMe: " + str(message.content)
+        cv.bot_append()
+        await message.author.send(cv.bot_name + cv.toks)
         response = await client.wait_for("message")
         print(f"Reponse: {response}")
-        await message.author.send("Got it! I will work on it right away.")
+        message = response
+
+    if message.content == "quit" and in_conversation:
+        in_conversation = False
+        print(f"Exiting Chat With {actor_name}")
+        print("Conversation: " + cv.environment)
+        return
 
 
 def run():
--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-poetry --directory /opt/pysetup shell
+#poetry --directory /opt/pysetup shell
 
 env_file="/workspace/config/.env"
+jargonai_env="/jargonai/config/.env"
+
 if [ ! -f "${env_file}" ]; then
     echo ".env file does not exist. Please create and add correct API keys."
     exit 1
 fi
 
 source ${env_file}
+source ${jargonai_env}
+
+#pushd /jargonai && poetry --directory /opt/pysetup install && popd
 
 python -m discord_bot
